0116af1ad8d7f4ce50b2659f815e3d85d7c1611d
Damjan Kojc <damjan.kojc@gmail.com>
2025-06-12 18:42:39 +0200

    JwtAuthenticationFilter in ActorsService modified and added in MoviesService, added ActorServiceImplementation

--- movies-service/src/main/java/si/src/bcc/movies/security/JwtAuthenticationFilter.java
+++ movies-service/src/main/java/si/src/bcc/movies/security/JwtAuthenticationFilter.java
@@ -1,4 +1,4 @@
-package si.src.bcc.actors.security;
+package si.src.bcc.movies.security;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
@@ -11,27 +11,27 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
-import si.src.bcc.actors.config.JwtConfig;
+import si.src.bcc.movies.config.JwtConfig;
 import javax.crypto.SecretKey;
 import java.io.IOException;
 import java.util.ArrayList;
 
 @Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
-    
+
     private final SecretKey jwtKey;
-    
+
     @Autowired
     public JwtAuthenticationFilter(JwtConfig jwtConfig) {
         this.jwtKey = jwtConfig.jwtKey();
     }
-    
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
-        
+
         String authHeader = request.getHeader("Authorization");
-        
+
         if (authHeader != null && authHeader.startsWith("Bearer ")) {
             String jwt = authHeader.substring(7);
             try {
@@ -40,18 +40,18 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
                         .build()
                         .parseClaimsJws(jwt)
                         .getBody();
-                
+
                 String username = claims.getSubject();
-                
+
                 UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
                         username, null, new ArrayList<>());
-                
+
                 SecurityContextHolder.getContext().setAuthentication(authentication);
             } catch (Exception e) {
                 SecurityContextHolder.clearContext();
             }
         }
-        
+
         filterChain.doFilter(request, response);
     }
 }
